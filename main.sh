e7194c2043be55882bed594a9b53703786afce68
yogeswar <yogeswar9573@gmail.com>
2026-04-29 02:56:23 +0530

    For main.sh colours are added

--- main.sh
+++ main.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Welcome to Gaming World"
-echo "Let go of stress,let the chill begin."
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}Welcome to the Mini Game Hub${NC}"
 
 #check_user function checks whether user registerd or not registered
 check_user() {
@@ -19,15 +24,15 @@ player_login() {
     local user=""
     local pass=""
 while true; do
-	#Enter player username
-	read -p "Enter player$player_no username: " user
+    #Enter player username
+    read -p "$(echo -e "${YELLOW}Enter player$player_no username: ${NC}")" user
 
 if check_user "$user"; then #user already registered
-        echo "Player already registered."
+        #echo -e "${GREEN}Player already registered.${NC}" 
 
 #Enter player password it will not be shown in terminal
-read -s -p "Enter player$player_no password: " pass 
-echo ""
+read -s -p "$(echo -e "${YELLOW}Enter player$player_no password: ${NC}")" pass 
+echo "" >&2
 
 #Converts our password into hashed password by using sha256 
 hashedpass=$(echo -n "${pass}" | sha256sum | cut -d ' ' -f1 )
@@ -35,30 +40,31 @@ hashedpass=$(echo -n "${pass}" | sha256sum | cut -d ' ' -f1 )
 #stored password is the sha256 password stored for the user in users.tsv
 storedpass=$(grep "^$user"$'\t' users.tsv | cut -d $'\t' -f2 )
 
-	if [[ ${hashedpass} == ${storedpass} ]]; then #If passwords match then login succesful
-            echo "${user} login succesful"
-            echo "$user"
+    if [[ ${hashedpass} == ${storedpass} ]]; then #If passwords match then login succesful
+            echo "" >&2
+            echo -e "${GREEN}${user} logged in succesfully${NC}" >&2
+            echo "$user" 
             return 0        
         else
-            echo "Invalid password" #If passwords doesn't match then try again
-            echo "Please try again"
+            echo -e "${RED}Invalid password!!${NC}" >&2 #If passwords doesn't match then try again
+            echo -e "${RED}< Please try again >${NC}" >&2
         fi
 else  #If user not registered
 
-	#Type Y if you want to register
-	#if you select otherthan Y it doesn't register
-        read -p "You want to register(Y/N): " select
+    #Type Y if you want to register
+    #if you select otherthan Y it doesn't register
+        read -p "$(echo -e "${YELLOW}Do you want to register ? (Y/N): ${NC}")" select
         if [[ "${select}" == "Y" ]]; then 
 
-		#Creates password for the user
-                read -s -p "Create password for $user " pass
-                echo "" 
+        #Creates password for the user
+                read -s -p "$(echo -e "${YELLOW}Create a password for $user ${NC}")" pass
+                echo "" >&2
                 echo -e "${user}\t$(echo -n "${pass}" | sha256sum | cut -d ' ' -f1 )" >> users.tsv #stores hashed password in users.tsv
-                echo "Your registration succesfull" 
+                echo -e "${GREEN}Your registration is succesfull${NC}" >&2
                 echo "$user"
                 return 0    
             else
-                echo "Player not registered"
+              echo -e "${RED}Player not registered${NC}" >&2
         fi
 fi
 done
@@ -70,11 +76,11 @@ while true; do
     
 #Ensures both players are different
     if [[ $player1_user != $player2_user ]]; then
-        echo "You both can start the game"
-        break	#If player1 and player2 are different then breaks this loop and call the game.py 
+        echo -e "${GREEN}You both can start the game${NC}" >&2
+        break   #If player1 and player2 are different then breaks this loop and call the game.py 
         else #If player1 and player2 are same then player2 need to login again
-        echo "Player1 and Player2 are same"
-        echo "Player2 need to login again"
+        echo -e "${RED}Player1 is same as player2${NC}" >&2
+        echo -e "${RED}Player2 need to login again${NC}" >&2
     fi
 done
 
